18a559998d9597302209537b0872ef5e2940b781
Geoffrey Owuor <120029033+jeffx-3@users.noreply.github.com>
2023-03-20 07:14:05 +0300

    Add files via upload

---
+++ file.kt
@@ -0,0 +1,91 @@
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import java.util.*
+import javax.mail.*
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
+
+@Composable
+fun EmailScreen() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var recipient by remember { mutableStateOf("") }
+    var subject by remember { mutableStateOf("") }
+    var message by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email address") },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email)
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password)
+        )
+        OutlinedTextField(
+            value = recipient,
+            onValueChange = { recipient = it },
+            label = { Text("Recipient") },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email)
+        )
+        OutlinedTextField(
+            value = subject,
+            onValueChange = { subject = it },
+            label = { Text("Subject") }
+        )
+        OutlinedTextField(
+            value = message,
+            onValueChange = { message = it },
+            label = { Text("Message") },
+            maxLines = 5
+        )
+        Button(onClick = { sendEmail(context, email, password, recipient, subject, message) }) {
+            Text("Send Email")
+        }
+    }
+}
+
+fun sendEmail(context: Context, email: String, password: String, recipient: String, subject: String, message: String) {
+    val props = Properties()
+    props["mail.smtp.auth"] = "true"
+    props["mail.smtp.starttls.enable"] = "true"
+    props["mail.smtp.host"] = "smtp.gmail.com"
+    props["mail.smtp.port"] = "587"
+
+    val session = Session.getInstance(props, object : Authenticator() {
+        override fun getPasswordAuthentication(): PasswordAuthentication {
+            return PasswordAuthentication(email, password)
+        }
+    })
+
+    try {
+        val mimeMessage = MimeMessage(session)
+        mimeMessage.setFrom(InternetAddress(email))
+        mimeMessage.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient))
+        mimeMessage.subject = subject
+        mimeMessage.setText(message)
+        Transport.send(mimeMessage)
+        Toast.makeText(context, "Email sent successfully!", Toast.LENGTH_SHORT).show()
+    } catch (e: Exception) {
+        e.printStackTrace()
+        Toast.makeText(context, "Failed to send email.", Toast.LENGTH_SHORT).show()
+    }
+}
